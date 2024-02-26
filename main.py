823973e2a9c943d4e7048de68f5436c15303627b
Sri Charan Thoutam <thoutamsricharan@gmail.com>
2024-02-26 22:31:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,118 @@
+# Importing necessary libraries
+import fastapi
+import joblib
+import pandas as pd
+import numpy as np
+from pydantic import BaseModel
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from collections import defaultdict
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
+
+app = fastapi.FastAPI()
+
+# Load the model and dataset
+song_cluster_pipeline = joblib.load('song_cluster_pipeline_model.joblib')
+spotify_data = pd.read_csv('datasets/data.csv')
+
+# Copy and paste cells 15-16 from main.ipynb file
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="PASTE YOUR SPOTIFY CLIENT ID",
+                                                           client_secret="PASTE YOUR SPOTIFY CLIENT SECRET KEY"))
+
+def find_song(name, year):
+    song_data = defaultdict()
+    results = sp.search(q= 'track: {} year: {}'.format(name,year), limit=1)
+    if results['tracks']['items'] == []:
+        return None
+
+    results = results['tracks']['items'][0]
+    track_id = results['id']
+    audio_features = sp.audio_features(track_id)[0]
+
+    song_data['name'] = [name]
+    song_data['year'] = [year]
+    song_data['explicit'] = [int(results['explicit'])]
+    song_data['duration_ms'] = [results['duration_ms']]
+    song_data['popularity'] = [results['popularity']]
+
+    for key, value in audio_features.items():
+        song_data[key] = value
+
+    return pd.DataFrame(song_data)
+
+number_cols = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
+ 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
+
+
+def get_song_data(song, spotify_data):
+    
+    try:
+        song_data = spotify_data[(spotify_data['name'] == song['name']) 
+                                & (spotify_data['year'] == song['year'])].iloc[0]
+        return song_data
+    
+    except IndexError:
+        return find_song(song['name'], song['year'])
+        
+
+def get_mean_vector(song_list, spotify_data):
+    
+    song_vectors = []
+    
+    for song in song_list:
+        song_data = get_song_data(song, spotify_data)
+        if song_data is None:
+            print('Warning: {} does not exist in Spotify or in database'.format(song['name']))
+            continue
+        song_vector = song_data[number_cols].values
+        song_vectors.append(song_vector)  
+    
+    song_matrix = np.array(list(song_vectors))
+    return np.mean(song_matrix, axis=0)
+
+
+def flatten_dict_list(dict_list):
+    
+    flattened_dict = defaultdict()
+    for key in dict_list[0].keys():
+        flattened_dict[key] = []
+    
+    for dictionary in dict_list:
+        for key, value in dictionary.items():
+            flattened_dict[key].append(value)
+            
+    return flattened_dict
+
+def recommend_songs( song_list, spotify_data, n_songs=10):
+    
+    metadata_cols = ['name', 'year', 'artists']
+    song_dict = flatten_dict_list(song_list)
+    
+    song_center = get_mean_vector(song_list, spotify_data)
+    scaler = song_cluster_pipeline.steps[0][1]
+    scaled_data = scaler.transform(spotify_data[number_cols])
+    scaled_song_center = scaler.transform(song_center.reshape(1, -1))
+    distances = cdist(scaled_song_center, scaled_data, 'cosine')
+    index = list(np.argsort(distances)[:, :n_songs][0])
+    
+    rec_songs = spotify_data.iloc[index]
+    rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
+    return rec_songs[metadata_cols].to_dict(orient='records')
+
+# Define the model for song input
+class SongInput(BaseModel):
+    name: str
+    year: int
+
+# Define the endpoint for recommendations
+@app.post("/recommendations")
+async def get_recommendations(song_input: SongInput):
+    recommendations = recommend_songs([song_input.dict()], spotify_data)
+    return recommendations
+
+# Start the server
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000) # Open your browser and go to http://127.0.0.1:8000/docs
